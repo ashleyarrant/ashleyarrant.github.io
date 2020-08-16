64cc9d78ebd6c179533dcbb8a263f277fe3f87a2
Ashley <ashleyarrant96@gmail.com>
2020-08-16 03:15:17 -0400

    updates

---
+++ scripts/config.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(document).ready(function (){
+    
+    AOS.init({
+        duration: 1200,
+        disable: 'mobile',
+    });
+    
+});

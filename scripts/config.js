360ac255d2fa9ccb7759081ba9d44993d2125fc5
Ashley <ashleyarrant96@gmail.com>
2020-09-03 05:17:59 -0400

    major updates

--- scripts/config.js
+++ scripts/config.js
@@ -10,3 +10,31 @@ $(document).ready(function (){
     });
     
 });
+
+function formgen() {
+    //gather input
+    var name = $("#sendername").val();
+    var subject = $("#emailsubject").val();
+    var msg = $("#emailbody").val();
+    
+    //encode them   
+    name = htmlsafe(name);
+    subject = htmlsafe(subject);
+    msg = htmlsafe(msg);
+    
+    window.open("mailto:ashleyarrant96@gmail.com?Subject="+subject+"&Body="+msg+"%0A%0ARegards%2C%0A"+name);
+
+    
+}
+
+function htmlsafe(x) {
+    x = x.replace(/ /g,"%20");
+    x = x.replace(/,/g,"%2C");
+    x = x.replace(/</g,"%3C");
+    x = x.replace(/>/g,"%3E");
+    x = x.replace(/!/g,"%21"); 
+    x = x.replace(/\?/g,"%3F");
+    x = x.replace(/\n/g,"%0A");
+    
+    return x;
+}
